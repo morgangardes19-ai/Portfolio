98e486133815c754ee4f350b3138301b948f7ff6
morgan1000 <morgan.gardes.19@gmail.com>
2026-07-23 16:50:30 +0200

    màj

--- _partials/_head.php
+++ _partials/_head.php
@@ -4,8 +4,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
+    <title>Morgan Gardès | Portfolio</title>
     <link href="../assets/styles/style.css" rel="stylesheet">
+    <link rel="icon" href="../assets/images/bulles-favicon-32px.png">
+    <!-- Police d'écriture : JetBrains Mono -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+    <!-- Police d'écriture : Inter -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <header class="fixed">
+        <!-- Ici : photo de profil -->
+        <div>
+            <div class="border border-radius-5">
+                <img src="" alt="photo de profil">
+            </div class="flex flex-row gap-2">
+            <p>Morgan Gardès</p>
+        </div>
+        <nav class="flex flex-row gap-2">
+            <div>
+                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Profil</p>
+            </div>
+            <div>
+                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Compétences</p>
+            </div>
+            <div>
+                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Projets</p>
+            </div>
+            <div>
+                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Contact</p>
+            </div>
+        </nav>
+    </header>
