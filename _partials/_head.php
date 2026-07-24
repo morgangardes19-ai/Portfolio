5b134e10c1802be7414690f4ecead22c4d09b99e
morgan1000 <morgan.gardes.19@gmail.com>
2026-07-24 13:48:03 +0200

    màj

--- _partials/_head.php
+++ _partials/_head.php
@@ -18,8 +18,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
 
-<body class="flex flex-col px-2 bg-bg-page-tempo">
-    <header class="fixed flex flex-row gap-32 items-center justify-between px-8 border border-border-header rounded-[50px] w-full backdrop-blur-xl">
+<body class="flex flex-col px-4 bg-bg-page-tempo">
+    <header class="fixed flex flex-row gap-32 items-center justify-between border border-border-header rounded-[50px] w-full backdrop-blur-xl">
         <!-- Ici : photo de profil -->
         <div class="flex flex-row gap-4 items-center ">
             <img src="../assets/images/photo-temporaire.jpg" class="rounded-[50%] border border-blue-500 w-16 h-16" alt="photo de profil">
@@ -29,8 +29,8 @@
         <!-- Partie droite header version Mobile -->
         <nav class="flex lg:hidden flex-row gap-2">
             <img id="logo-burger" src="../assets/images/menu-burger.png" alt="menu hamburger">
-            <div id="overlay-burger">
-                <img id="croix-menu-burger" src="../assets/images/croix-32px.png" alt="logo croix">
+            <div id="overlay-burger" class="fixed transition-all duration-300 translate-x-full top-16.5 right-0 flex flex-col items-center gap-6 px-16 py-14 text-boutons-header text-[16px] ibm-plex-mono-regular bg-black/95 rounded-md tracking-[10%] text-center">
+                <img id="croix-menu-burger" class="absolute top-6 right-6" src="../assets/images/croix-32px.png" alt="logo croix">
                 <a class="text-gris-texte-header hover:text-bleu-texte-hover-header" href="">Profil</a>
 
                 <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Compétences</p>
