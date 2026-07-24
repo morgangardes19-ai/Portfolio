788c87a184d2e4d7f5e74c517d484c051be82b95
morgan1000 <morgan.gardes.19@gmail.com>
2026-07-24 16:56:10 +0200

    màj

--- _partials/_head.php
+++ _partials/_head.php
@@ -19,17 +19,17 @@
 </head>
 
 <body class="relative flex flex-col px-4 bg-bg-page-tempo">
-    <header class="fixed flex flex-row gap-32 items-center justify-between border border-border-header rounded-[50px] backdrop-blur-xl top-4 left-4 right-4 px-4">
+    <header class="fixed flex flex-row gap-32 items-center justify-between border border-border-header rounded-[50px] backdrop-blur-xl top-4 left-4 right-4 px-4 py-2">
         <!-- Ici : photo de profil -->
         <div class="flex flex-row gap-4 items-center ">
-            <img src="../assets/images/photo-temporaire.jpg" class="rounded-[50%] border border-blue-500 w-16 h-16" alt="photo de profil">
+            <img src="../assets/images/photo-temporaire.jpg" class="rounded-[50%] border border-blue-500 w-10 h-10" alt="photo de profil">
             <a href=""></a>
             <p class="hidden text-nom-profil-header lg:flex">Morgan Gardès</p>
         </div>
         <!-- Partie droite header version Mobile -->
         <nav class="flex lg:hidden flex-row gap-2 ">
             <img id="logo-burger" src="../assets/images/menu-burger.png" alt="menu hamburger">
-            <div id="overlay-burger" class="fixed transition-all duration-300 translate-x-full top-16.5 right-0 flex flex-col items-center gap-6 px-16 py-14 text-boutons-header text-[16px] inter-regular bg-black/80 rounded-md tracking-[10%] text-center">
+            <div id="overlay-burger" class="fixed transition-all duration-300 translate-x-full top-16.5 -right-4 flex flex-col items-center gap-6 px-16 py-14 text-boutons-header text-[16px] inter-regular bg-black/80 rounded-md tracking-[10%] text-center">
                 <img id="croix-menu-burger" class="absolute top-6 right-6" src="../assets/images/croix-32px.png" alt="logo croix">
                 <a class="text-gris-texte-header hover:text-bleu-texte-hover-header" href="">Profil</a>
 
