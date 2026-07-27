c8c052de51984dd4f93e373b65ffbeeac8894f5b
morgan1000 <morgan.gardes.19@gmail.com>
2026-07-27 12:00:17 +0200

    màj

--- _partials/_head.php
+++ _partials/_head.php
@@ -7,6 +7,8 @@
     <title>Morgan Gardès | Portfolio</title>
     <link href="../assets/styles/style.css" rel="stylesheet">
     <script src="../assets/scripts/overlay-menu-burger-mobile.js" defer></script>
+    <script src="../assets/scripts/header-scroll.js" defer></script>
+
     <link rel="icon" href="../assets/images/bulles-favicon-32px.png">
     <!-- Police d'écriture : JetBrains Mono -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -18,47 +20,49 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
 
-<body class="relative flex flex-col px-4 bg-bg-page-tempo">
-    <header class="fixed flex flex-row gap-32 items-center justify-between border border-border-header rounded-[50px] backdrop-blur-xl top-4 left-4 right-4 px-4 py-2">
-        <!-- Conteneur du sonar -->
-        <div class="relative flex items-center justify-center">
-            <!-- Anneau du sonar -->
-            <div class="absolute inset-0 rounded-full border-2 border-hover-cercle-profil
-                animate-[sonar-pulse_2s_ease-out_infinite]"></div>
-            <!-- Photo de profil -->
-            <div class="flex flex-row gap-4 items-center ">
-                <img src="../assets/images/photo-temporaire.jpg" class="relative z-10 rounded-full border-border-photo-profil w-14 h-14 hover:border-hover-cercle-profil border-2" alt="photo de profil">
-                <a href=""></a>
-                <p class="hidden text-nom-profil-header lg:flex">Morgan Gardès</p>
-            </div>
-        </div>
-        <!-- Partie droite header version Mobile -->
-        <nav class="flex lg:hidden flex-row gap-2">
-            <img id="logo-burger" src="../assets/images/menu-burger.png" alt="menu hamburger">
-            <div id="overlay-burger" class="fixed transition-all duration-300 translate-x-full top-16.5 -right-4 flex flex-col items-center gap-6 px-16 py-14 text-boutons-header text-[16px] inter-regular bg-black/80 rounded-md tracking-[10%] text-center">
-                <img id="croix-menu-burger" class="absolute top-6 right-6" src="../assets/images/croix-32px.png" alt="logo croix">
-                <a class="text-gris-texte-header hover:text-bleu-texte-hover-header" href="">Profil</a>
+<body class="">
+    <div class="relative min-h-screen overflow-x-hidden caustic-overlay">
 
-                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Compétences</p>
+        <header id="header" class="fixed flex flex-row gap-32 items-center justify-between top-4 left-4 right-4 px-4 py-2 ">
+            <!-- Conteneur du sonar -->
+            <div class="relative flex items-center justify-center w-14 h-14">
+                <!-- Anneau du sonar -->
+                <div class="absolute inset-0 rounded-full border border-hover-cercle-profil
+                    animate-[sonar-pulse_2s_ease-out_infinite]"></div>
+                <!-- Photo de profil -->
+                <div class="flex flex-row gap-4 items-center ">
+                    <img src="../assets/images/photo-temporaire.jpg" class="relative z-10 rounded-full border-border-photo-profil w-14 h-14 hover:border-hover-cercle-profil border-2" alt="photo de profil">
+                    <a href=""></a>
+                    <p class="hidden text-nom-profil-header lg:flex">Morgan Gardès</p>
+                </div>
+            </div>
+            <!-- Partie droite header version Mobile -->
+            <nav class="flex lg:hidden flex-row gap-2">
+                <img id="logo-burger" src="../assets/images/menu-burger.png" alt="menu hamburger">
+                <div id="overlay-burger" class="fixed transition-all duration-300 translate-x-full top-16.5 -right-4 flex flex-col items-center gap-6 px-16 py-14 text-boutons-header text-[16px] inter-regular bg-black/80 rounded-md tracking-[10%] text-center">
+                    <img id="croix-menu-burger" class="absolute top-6 right-6" src="../assets/images/croix-32px.png" alt="logo croix">
+                    <a class="text-gris-texte-header hover:text-bleu-texte-hover-header" href="">Profil</a>
 
-                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Projets</p>
+                    <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Compétences</p>
 
-                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Contact</p>
-            </div>
-        </nav>
-        <!-- Partie droite header version PC -->
-        <nav class="hidden lg:flex flex-row gap-2 text-[20px]">
-            <div>
-                <a class="text-gris-texte-header hover:text-bleu-texte-hover-header" href="">Profil</a>
-            </div>
-            <div>
-                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Compétences</p>
-            </div>
-            <div>
-                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Projets</p>
-            </div>
-            <div>
-                <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Contact</p>
-            </div>
-        </nav>
-    </header>
+                    <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Projets</p>
+
+                    <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Contact</p>
+                </div>
+            </nav>
+            <!-- Partie droite header version PC -->
+            <nav class="hidden lg:flex flex-row gap-2 text-[20px]">
+                <div>
+                    <a class="text-gris-texte-header hover:text-bleu-texte-hover-header" href="">Profil</a>
+                </div>
+                <div>
+                    <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Compétences</p>
+                </div>
+                <div>
+                    <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Projets</p>
+                </div>
+                <div>
+                    <p class="text-gris-texte-header hover:text-bleu-texte-hover-header">Contact</p>
+                </div>
+            </nav>
+        </header>
--- public/index.php
+++ public/index.php
@@ -1,40 +1,153 @@
 <?php require_once "../_partials/_head.php" ?>
 
-<main class="pt-40">
 
-    <div>
-        <div>
-            <p>// 01</p>
-            <h1 class="texte-[64px] text-white">MORGAN GARDÈS</h1>
-            <h3>Développeur Web</h3>
-            <p>Passionné par le développement web, je transforme des idées en expériences numériques fluides du premier pixel jusqu'au déploiement. Curieux et rigoureux, je plonge avec plaisir dans de nouveaux défis techniques. Chaque projet est une exploration vers plus de profondeur et de précision.</p>
-        </div>
-        <div>
-            <p>Frontend</p>
-            <p>Backend</p>
-            <p>Performance</p>
-            <p>Responsive</p>
-        </div>
-    </div>
 
-    <p>// 02</p>
-    <h2>COMPÉTENCES</h2>
-<!-- Lien de téléchargement du CV -->
+    <main class="pt-40">
+
+        <div>
+            <div>
+                <p>// 01</p>
+                <h1 class="texte-[64px] text-white">MORGAN GARDÈS</h1>
+                <h3>Développeur Web</h3>
+                <p>Passionné par le développement web, je transforme des idées en expériences numériques fluides du premier pixel jusqu'au déploiement. Curieux et rigoureux, je plonge avec plaisir dans de nouveaux défis techniques. Chaque projet est une exploration vers plus de profondeur et de précision.</p>
+            </div>
+            <div>
+                <p>Frontend</p>
+                <p>Backend</p>
+                <p>Performance</p>
+                <p>Responsive</p>
+            </div>
+        </div>
+
+        <p>// 02</p>
+        <h2>COMPÉTENCES</h2>
+        <!-- Lien de téléchargement du CV -->
+
+        <div class="flex flex-row gap-4">
+            <!-- Bulles des différents Skills -->
+            <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+            <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+            <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+            <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+            <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+            <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+        </div>
+
+        <p>// 03</p>
+        <p>// Cliquez sur une bulle pour la faire éclater</p>
+        <p>Survolez un viewport pour explorer l'intégralité du site.</p>
+
+        <h2>PROJETS</h2>
+
+    </main>
+
+
 
 <div class="flex flex-row gap-4">
     <!-- Bulles des différents Skills -->
-     <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
-     <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
-     <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
-     <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
-     <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
-     <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
 </div>
-
-<p>// 03</p>
-<p>// Cliquez sur une bulle pour la faire éclater</p>
-<p>Survolez un viewport pour explorer l'intégralité du site.</p>
-
-<h2>PROJETS</h2>
-
-</main>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
+<div class="flex flex-row gap-4">
+    <!-- Bulles des différents Skills -->
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+    <div class=" rounded-[50%] border border-blue-700 bg-blue-500 w-16 h-16"></div>
+</div>
